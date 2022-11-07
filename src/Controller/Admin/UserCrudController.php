3a2fc1e8828a801a6baccc1c4d46c120d270ca4b
Deherder Michel <webmaster@cyel.be>
2022-11-07 20:05:14 +0100

    Configure avatar, an upload Field. Install symfony/mime

--- src/Controller/Admin/UserCrudController.php
+++ src/Controller/Admin/UserCrudController.php
@@ -9,6 +9,7 @@ use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\EmailField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
 class UserCrudController extends AbstractCrudController
@@ -22,6 +23,10 @@ class UserCrudController extends AbstractCrudController
     {
         yield IdField::new('id')
             ->onlyOnIndex();
+        yield ImageField::new('avatar')
+            ->setBasePath('uploads/avatars')
+            ->setUploadDir('public/uploads/avatars')
+            ->setUploadedFileNamePattern('[slug]-[timestamp].[extension]');
         yield EmailField::new('email');
         yield TextField::new('firstName')
             ->onlyOnForms();
