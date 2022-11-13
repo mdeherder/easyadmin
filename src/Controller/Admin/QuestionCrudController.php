6d309fd23bf1dd095514a65bf282a7ba3c19ac5a
Deherder Michel <webmaster@cyel.be>
2022-11-13 21:43:50 +0100

    Configure Filter

--- src/Controller/Admin/QuestionCrudController.php
+++ src/Controller/Admin/QuestionCrudController.php
@@ -8,15 +8,14 @@ use Doctrine\ORM\QueryBuilder;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Filters;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\Field;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 
-#[IsGranted('ROLE_SUPER_ADMIN')]
 class QuestionCrudController extends AbstractCrudController
 {
     public static function getEntityFqcn(): string
@@ -50,11 +49,13 @@ class QuestionCrudController extends AbstractCrudController
             ])
             ->setHelp('Preview:')
         ;
-        yield VotesField::new('votes', 'Total Votes');
+        yield VotesField::new('votes', 'Total Votes')
+            ->setPermission('ROLE_SUPER_ADMIN')
+        ;
         yield AssociationField::new('askedBy')
             ->autocomplete()
-            ->formatValue(static function ($value, Question $question): ?string {
-                if (!$user = $question->getAskedBy()) {
+            ->formatValue(static function ($value, ?Question $question): ?string {
+                if (!$user = $question?->getAskedBy()) {
                     return null;
                 }
 
@@ -94,6 +95,16 @@ class QuestionCrudController extends AbstractCrudController
             ->setPermission(Action::NEW, 'ROLE_SUPER_ADMIN')
             ->setPermission(Action::DELETE, 'ROLE_SUPER_ADMIN')
             ->setPermission(Action::BATCH_DELETE, 'ROLE_SUPER_ADMIN')
+        ;
+    }
+
+    public function configureFilters(Filters $filters): Filters
+    {
+        return parent::configureFilters($filters)
+            ->add('topic')
+            ->add('createdAt')
+            ->add('votes')
+            ->add('name')
         ;
     }
 }
